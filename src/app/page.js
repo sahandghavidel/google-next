f3afa4ec0dba8fcde308719db50478eeb2716762
Sahand Ghavidel <sahand.gha@gmail.com>
2024-01-20 08:53:40 +0700

    add home header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
+import HomeHeader from '@/components/HomeHeader';
+
 export default function Home() {
   return (
-    <h1 className='text-red-500'>Home</h1>
-  )
+    <>
+      <HomeHeader />
+    </>
+  );
 }
