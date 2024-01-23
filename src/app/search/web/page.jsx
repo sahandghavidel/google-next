0fd020151b33171fb6d1dc655219f1728471f2e2
Sahand Ghavidel <sahand.gha@gmail.com>
2024-01-23 14:33:08 +0700

    Fetch data from google search API and show the titles

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,10 @@
-export default function WebSearchPage() {
+export default async function WebSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}`
+  );
+  const data = await response.json();
+  const results = data.items;
   return (
-    <div>WebSearchPage</div>
-  )
+    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
+  );
 }
