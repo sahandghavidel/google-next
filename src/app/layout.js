39e7cf6f4299c850d0659fffbdc21037ad7c8c53
Sahand Ghavidel <sahand.gha@gmail.com>
2024-01-24 09:05:38 +0700

    create search result component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
-import { Inter } from 'next/font/google';
 import './globals.css';
 import Footer from '@/components/Footer';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Google Clone',
@@ -13,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body className='relative min-h-screen'>
         {children}
         <Footer />
       </body>
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,4 @@
+import WebSearchResults from '@/components/WebSearchResults';
 import Link from 'next/link';
 
 export default async function WebSearchPage({ searchParams }) {
@@ -24,7 +25,5 @@ export default async function WebSearchPage({ searchParams }) {
     );
   }
 
-  return (
-    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
-  );
+  return <div>{results && <WebSearchResults results={data} />}</div>;
 }
